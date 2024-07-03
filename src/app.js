ca670dd10b67335ea5f3eff8efcc9056cc2c0d3a
Adolfo Galué <132372341+Adolfo1996@users.noreply.github.com>
2024-07-03 16:22:46 +0000

    generador de cartas aleatorias.

--- src/app.js
+++ src/app.js
@@ -4,8 +4,34 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
+let suits = ["♥", "♣", "♦", "♠"];
+let numbers = [
+  "A",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+];
+let topSuit = document.querySelector("#card-top-suit");
+let number = document.querySelector("#card-number");
+let bottomSuit = document.querySelector("#card-bottom-suit");
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  newCard();
+};
+
+const newCard = () => {
+  topSuit.innerHTML = suits[Math.floor(Math.random() * suits.length)];
+  number.innerHTML = numbers[Math.floor(Math.random() * numbers.length)];
+  bottomSuit.innerHTML = topSuit.innerHTML;
 };
